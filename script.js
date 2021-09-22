aaabed59a3d07737d08b50b62e9b5008765175b1
HCTFuzz <rmccallie@hotmail.com>
2021-09-22 09:25:04 -0400

    added initial files

---
+++ script.js
@@ -0,0 +1,28 @@
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var length = prompt("Please choose password length", "Enter numerical value between 8 - 128");
+  if (length = "8" - "128") {
+      prompt("Please enter valid numerical range");
+        }
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  
+
+  // passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);{
+  
+// }
+
